85f9016b8ea379e5cc58bc5401555cddbc99aa2b
PHDevss <97169087+PHDevss@users.noreply.github.com>
2022-04-01 16:56:19 -0300

    Avanço Projeto

--- app.js
+++ app.js
@@ -1,10 +1,43 @@
 const express = require("express");
+const res = require("express/lib/response");
 
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', function (req, res) {
-    res.send('Hello')    
+    let today = new Date()
+    let currentDay = today.getDay()
+    let day = ''
+    switch (currentDay) {
+        case 0: 
+            day = 'Domingo'
+            break
+        case 1: 
+            day = 'Segunda-feira'
+            break
+        case 2: 
+            day = 'Terça-feira'
+            break
+        case 3: 
+            day = 'Quarte-feira'
+            break
+        case 4: 
+            day = 'Quinta-feira'
+            break
+        case 5: 
+            day = 'Sexta-feira'
+            break
+        case 6: 
+            day = 'Sábado'
+            break
+        default:
+            console.log(Day + ' inválido!')
+    }
+    res.render('list', {kindOfDay: day})
 })
+
+
 
 app.listen(3000, function () {
     console.log('Server');
