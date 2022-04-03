a2ac56144c93f756a35a3c425307fbde0c23489a
PHDevss <97169087+PHDevss@users.noreply.github.com>
2022-04-03 10:32:17 -0300

    Finalizando EJS

--- app.js
+++ app.js
@@ -2,41 +2,34 @@ const express = require("express");
 const res = require("express/lib/response");
 
 const app = express()
+let itens = []
 
 app.set('view engine', 'ejs')
 
+app.use(express.urlencoded({
+    extended: true
+}))
+
+
 app.get('/', function (req, res) {
     let today = new Date()
-    let currentDay = today.getDay()
-    let day = ''
-    switch (currentDay) {
-        case 0: 
-            day = 'Domingo'
-            break
-        case 1: 
-            day = 'Segunda-feira'
-            break
-        case 2: 
-            day = 'Terça-feira'
-            break
-        case 3: 
-            day = 'Quarte-feira'
-            break
-        case 4: 
-            day = 'Quinta-feira'
-            break
-        case 5: 
-            day = 'Sexta-feira'
-            break
-        case 6: 
-            day = 'Sábado'
-            break
-        default:
-            console.log(Day + ' inválido!')
+    
+    const options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
     }
-    res.render('list', {kindOfDay: day})
+    
+    let day = today.toLocaleDateString('pt-BR', options)
+
+    res.render('list', { kindOfDay: day, itemAdicionado: itens })
 })
 
+app.post('/', function(req, res){
+    let item = req.body.novoItem
+    itens.push(item)
+    res.redirect('/')
+})
 
 
 app.listen(3000, function () {
