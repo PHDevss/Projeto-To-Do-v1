18f12948f285fc0a3b174f20afc74c97c9a0236f
PHDevss <97169087+PHDevss@users.noreply.github.com>
2022-04-23 17:44:50 -0300

    Introdução Backend MongoDB

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const res = require("express/lib/response");
-const date = require(__dirname+'/date.js')
+// const date = require(__dirname+'/date.js')
+const mongoose = require('mongoose')
 
 const app = express()
 const itens = []
-const workItens = []
+// const workItens = []
 
 app.set('view engine', 'ejs')
 
@@ -13,26 +14,64 @@ app.use(express.urlencoded({
 }))
 app.use(express.static('public'))
 
+//BANCO DE DADOS 
+mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true})
+
+const itemsSchema = {
+    name: String
+}
+
+const Item = mongoose.model("Item", itemsSchema)
+
+
+
+//END BANCO DE DADOS
 
 app.get('/', function (req, res) {
-    const day = date.getDate()
-    res.render('list', { listTitle: day, itemAdicionado: itens })
+    // const day = date.getDate()
+    Item.find({},function(err, foundItens) {
+        if(err){
+            console.log(err);
+        }else{
+            if(foundItens.length === 0){
+                const item1 = new Item ({
+                    name: 'Welcome to your todoList!'
+                })
+                const item2 = new Item ({
+                    name: 'Hit the + button to add a new item.'
+                })
+                const item3 = new Item ({
+                    name: '<== Hit this to delete an item.'
+                })
+                
+                const defaultItems = [item1, item2, item3]
+                
+                Item.insertMany(defaultItems, function(err){
+                    if(err){
+                        console.log(err)
+                    } else {
+                        console.log('Successfully saved default items to DB.');
+                    }
+                })
+            }
+
+            foundItens.forEach(element => {
+                itens.push(element.name)
+            });
+            res.render('list', { listTitle: "Today", itemAdicionado: itens })
+        }
+    })
+
 })
 
-
-app.get('/work', function (req,res) {
-    res.render('list', {listTitle: "Work List", itemAdicionado: workItens})
-})
 
 app.post('/', function(req, res){
-    const item = req.body.novoItem
-    if(req.body.list === 'Work') {
-        workItens.push(item)
-        res.redirect('/work')
-    } else {
-        itens.push(item)
-        res.redirect('/')
-    }
+    const itemName = req.body.novoItem
+    const newItem = new Item ({
+        name: itemName
+    })
+    newItem.save()
+    res.redirect('/')
 })
 
 
