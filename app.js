60223c598b0227b4f51b31b85ad27fd6bae7800a
PHDevss <97169087+PHDevss@users.noreply.github.com>
2022-05-03 17:21:25 -0300

    Update Heroku

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 })
 
 //BANCO DE DADOS 
-mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true})
+mongoose.connect('mongodb+srv://admin-phfoliveira:Prozy123@cluster0.xbyn2.mongodb.net/todolistDB', {useNewUrlParser: true})
 
 const itemsSchema = {
     name: String
